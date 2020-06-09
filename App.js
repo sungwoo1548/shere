e1dfa0a13125b277bb7c5ec0092a355221cac3bf
swo1548 <swo1548@gmail.com>
2020-06-09 15:26:21 +0900

    add&set firebase

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -9,8 +9,25 @@ import LinkingConfiguration from './navigation/LinkingConfiguration';
 
 const Stack = createStackNavigator();
 
+// firebase
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlJCsyG7njJfO1ahFE-6YIaVUVriMzI60",
+  authDomain: "shere-kor.firebaseapp.com",
+  databaseURL: "https://shere-kor.firebaseio.com",
+  projectId: "shere-kor",
+  storageBucket: "shere-kor.appspot.com",
+  messagingSenderId: "343473017119",
+  appId: "1:343473017119:web:adcddbfc4950b94e0d202a",
+  measurementId: "G-TRSDW30MTR"
+};
+
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
